120a30e40286a4c189e63208e2287e085d60eac2
kngys <kristiangys@gmail.com>
2024-09-28 01:43:50 +0800

    Update RemarkCommand and tests

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -22,13 +22,16 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
     public RemarkCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
         Index index;
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive);
         }
+
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+
         return new RemarkCommand(index, new Remark(remark));
     }
 }
